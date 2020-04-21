38992c75cd96cbeca5929871e1dfd9996316c54f
Sachin Sahu <Sachin.Sahu@inpixon.com>
2020-04-21 16:01:28 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,51 +4,9 @@ const geoCode = require("./util/geocode");
 const forecast = require("./util/forecast");
 const publicDirectoryPath = path.join(__dirname, "../public");
 const app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.static(publicDirectoryPath));
 
-// app.get("", (req, res) => {
-//   res.send("<h1>hello express</h1>");
-// });
-
-// app.get("/help", (req, res) => {
-//   res.send("help section");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("About section");
-// });
-
-app.get("/weather", (req, res) => {
-  console.log(req.query);
-  if (!req.query.location) {
-    return res.send({
-      error: "Kindly provide the location!",
-    });
-  }
-
-  geoCode(req.query.location, (error, { latitude, longitutde, place } = {}) => {
-    if (error) {
-      return res.send({
-        error,
-      });
-    }
-    forecast(latitude, longitutde, (error, { desc, temp } = {}) => {
-      if (error) {
-        return res.send({
-          error,
-        });
-      }
-
-      res.send({
-        desc,
-        temp,
-        place,
-      });
-    });
-  });
-});
-
-app.listen(3000, () => {
-  console.log("application is listing on 3000 port!");
+app.listen(port, () => {
+  console.log("application is listing on 3000 port!" + port);
 });
